bf0bbbb7e3479d387617f0265bf1f2a34f0316b8
shakil <me.mahadi.hasan.shakil@gmail.com>
2026-05-10 23:48:11 +0600

    chapter-1: solve excercise 1.12

--- chapter-1/chapter-1.c
+++ chapter-1/chapter-1.c
@@ -1,38 +1,7 @@
-#include<stdio.h>
-#include<ctype.h>
-
-#define YES 1
-#define NO 0
+#include <stdio.h>
 
 int main()
 {
-    int c, nl, nw, nc, inword;
-
-    inword = NO;
-    nl = nw = nc = 0;
-    while((c = getchar()) != EOF) {
-        ++nc;
-        if(c == '\n') {
-            printf("%d\n", nw);
-            nw = 0;
-        }
-
-        if(!isalpha(c)) continue;
-
-        while((c = getchar()) != EOF) {
-            if(isalpha(c) || isdigit(c) || c == '\'') continue;
-            if(c == ' ' || c == '\t') {
-                nw++;
-                break;
-            }
-            if(c == '\n') {
-                printf("%d\n", nw);
-                nw = 0;
-            }
-            break;
-        }
-
-    }
-
+     
     return 0;
 }
