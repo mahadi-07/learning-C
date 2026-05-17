9323b4d69c7ae6f8d7471338f9369666ac0488e7
shakil <me.mahadi.hasan.shakil@gmail.com>
2026-05-17 21:37:46 +0600

    exercise solve 2.2

--- chapter-2/chapter-2.c
+++ chapter-2/chapter-2.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
 
-int htoi(int ch)
-{
-    if(ch >= '0' && ch <= '9') return ch-'0';
-    else if(ch >= 'a' && ch <= 'z') return ch-'a'+10;
-    else return ch-'A'+10;
+int hexToInt(char *hex) {
+    // printf("%s\n", *hex++);
+    int val = 0;
+    while (*hex) {
+        // Get the value of the current hex digit
+        char byte = *hex;
+        hex++;
+
+        // char byte = *hex++;
+        if (byte >= '0' && byte <= '9') byte = byte - '0';
+        else if (byte >= 'a' && byte <= 'f') byte = byte - 'a' + 10;
+        else if (byte >= 'A' && byte <= 'F') byte = byte - 'A' + 10;
+        
+        // Shift existing value and add current digit
+        // printf("%d\b", 0xF);
+        // printf("%d %d %d %d\n", val, (val << 4), byte, (byte & 0xF));
+        val = (val << 4) | (byte & ((1<<4)-1));
+    }
+    return val;
 }
 
 int main()
 {
-    printf("%d\n", htoi('1'));
-    printf("%d\n", htoi('a'));
-    printf("%d\n", htoi('A'));
+    printf("%d\n", hexToInt("1FA"));
 
     return 0;
 }
